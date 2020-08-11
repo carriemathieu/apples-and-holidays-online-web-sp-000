8cb22ad8038aaf98a1a592c669ab3fc87df2b467
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 17:36:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,21 +18,82 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
+    # given that holiday_hash looks like this:
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
+holiday_hash[:winter][:christmas] << "Balloons"
+holiday_hash[:winter][:new_years] << "Balloons"
 
+return holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  
+   # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+#holiday_hash.each do |holiday, supply|
+ # supply.each do |attribute| #, value|
+  #  if attribute == :spring 
+   #   [:memorial_day] << "Grill"
+    #elsif attribute == :memorial_day
+     # [:memorial_day] << "Table Cloth"
+    #end
+   #end
+ #end
+#return holiday_hash
+  count = 0
+  holiday_hash.each do |holiday, data|
+    while count <= 2
+      if count == 0
+        holiday_hash[:spring][:memorial_day][1] = "Grill"
+        puts holiday_hash[:spring]
+        count +=1
+      elsif count == 1
+        holiday_hash[:spring][:memorial_day][1] = "Table Cloth"
+        puts holiday_hash[:spring]
+        count += 1
+      end
+    end
+  end
 end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here

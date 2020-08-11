cdebe1becd7df26374082e2308e816cb7299d9a3
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 19:30:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -139,7 +139,9 @@ def all_holidays_with_bbq(holiday_hash)
   #     :memorial_day => ["BBQ"]
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.each do |season, holiday|
+  season.each do |key, value|
+    
 end
 
 

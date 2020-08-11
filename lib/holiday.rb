dc8d423e2f881e74e62c776254dd7d4387c14288
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 19:27:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -108,6 +108,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
  holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supply|

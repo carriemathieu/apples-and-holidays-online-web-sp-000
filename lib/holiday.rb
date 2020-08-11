9df374aeb3671a4da1df19798fbff3857957aa9e
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 17:57:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash.values.flatten
+  return holiday_hash.values.flatten
 
 end
 

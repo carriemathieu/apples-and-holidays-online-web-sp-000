88a1375f33f1cd921729f212953d26379ad23ce6
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 18:13:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,6 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   return holiday_hash
 end
 
-
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],

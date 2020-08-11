153d6b627438c6a323ad38d1e959873323e400d4
Carrie Mathieu <carrieburks@gmail.com>
2020-08-11 18:01:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  holiday_hash[:fall][:columbus_day]
+  holiday_hash[:fall] = :columbus_day
   return holiday_hash
 end
 
